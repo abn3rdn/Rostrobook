7f57030aaffce177972283186a06212f80209704
Abner Rojas <abner.duran@gmail.com>
2014-09-10 12:27:30 -0500

    Creado el proyecto y agregado status al mismo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rostrobook::Application.config.secret_token = '75318fc8b4772fa9da415cfec3c659905909f0ac3182dee0036b873f2e01bc5d5f65e5bab579acffccea2cc4c4dc25b0796209d7e51f030dbbc7d4b88a822987'
